b83d56926d5229b42a60121bdbfa2ea5bfcaa579
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-02-25 13:51:23 -0300

    Pacote NewtonSoft.Json instalado

--- ConversorDeMoedas/FrmCotacaoDolar.cs
+++ ConversorDeMoedas/FrmCotacaoDolar.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +26,11 @@ namespace ConversorDeMoedas
         private void btn_pesquisar_Click(object sender, EventArgs e)
         {
             string strURL = "https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,USD&key=733fd8ea";
+
+            using (HttpClient client = new HttpClient())
+            {
+                
+            }
         }
     }
 }

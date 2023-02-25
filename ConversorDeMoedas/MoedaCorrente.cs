c3f12c94d272f825004f49560b6fa0242158a0cd
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-02-25 19:11:10 -0300

    Criação da classe MoedaCorrente, e as suas propriedades, e realizamos o JsonProperty para cada propriedade

---
+++ ConversorDeMoedas/MoedaCorrente.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoedas
+{
+    public class MoedaCorrente
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Nome { get; set; }
+
+        [JsonProperty(PropertyName = "buy")]
+        public decimal Compra { get; set; }
+
+        [JsonProperty(PropertyName = "sell")]
+        public decimal Venda { get; set;}
+
+        [JsonProperty(PropertyName = "variation")]
+        public decimal Variacao { get; set; }
+
+
+    }
+}
